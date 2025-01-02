3ddda95699599851f4f2da34734c411a5b354c2d
Aatmik-06 <adi120kush@gmail.com>
2025-01-02 20:00:16 +0530

    ai chatbot using Next js

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,93 @@
+'use client'
 import Image from "next/image";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from "react";
+import axios from "axios";
+import ReactMarkdown from "react-markdown";
+import { Vortex } from "@/components/ui/vortex";
+import Container from "react-bootstrap/Container";
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+   // Created state for question and answer
+   const [question, setQuestion] = useState("");
+   const [displayquestion, setDisplayquestion] = useState("");
+   const [answer, setAnswer]= useState("");
+ 
+  
+ 
+   // Created function to generate answer
+   
+   async function generatedanswer() {
+     setAnswer("Thinking...")
+     try {
+       
+       const response = await axios({
+         url: "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyAmI8sN1T5SzP3AlM-5CPxFRlxFUyxJvd0",
+         method:"post",
+         data:{
+             contents: [
+               { parts: [{text:question}]},],
+         },
+       });
+       setAnswer(response['data']['candidates'][0]['content']['parts'][0]['text']);
+       setQuestion("")
+       setDisplayquestion(question)
+       
+      
+    //  document.getElementById('question').style.display="block"
+     
+     } 
+      catch (error) {
+       console.log(error);
+       setAnswer("Sorry - Something went wrong. Please try again!");
+     }
+   }
+   
+   if(answer==""){
+     setAnswer("Hey there! I'm Genie AI Chatbot. How can I help you today?")
+   }
+   return (
+     <>
+   <Container fluid id='cont'>
+      <Vortex 
+        backgroundColor="black"
+        rangeY={800}
+        particleCount={500}
+        baseHue={600}
+        className="flex items-center flex-col justify-center w-full h-full"
+      >
+         <div id='main'>
+      <h1>Genie AI Chatbot</h1>
+      <div>
+      <div id="ans"  className="flex-1 overflow-y-auto mb-4 rounded-lg shadow-lg p-4 hide-scrollbar" >
+       <h3 style={{display:"none"}} id='question'> Question: {displayquestion} </h3>
+       
+       <ReactMarkdown  className="overflow-auto hide-scrollbar items-center">   
+       
+       {answer}
+ 
+       </ReactMarkdown>
+      
+      
+      </div>
+      </div>
+      <textarea value={question} id='input'  onChange={(e) => setQuestion(e.target.value)} placeholder="Ask anything..." ></textarea> <br /><br />
+     
+      <div>
+      
+      <button className="btn" onClick={generatedanswer}> 
+      <svg height="24" width="24" fill="#FFFFFF" viewBox="0 0 24 24" data-name="Layer 1" id="Layer_1" className="sparkle">
+         <path d="M10,21.236,6.755,14.745.264,11.5,6.755,8.255,10,1.764l3.245,6.491L19.736,11.5l-6.491,3.245ZM18,21l1.5,3L21,21l3-1.5L21,18l-1.5-3L18,18l-3,1.5ZM19.333,4.667,20.5,7l1.167-2.333L24,3.5,21.667,2.333,20.5,0,19.333,2.333,17,3.5Z"></path>
+     </svg>
+     <span className="text">Generate</span>
+   
+     </button>
+     
+      </div>
+      </div>
+     
+      
+       </Vortex>
+       </Container>
+     </>
+   )
 }
