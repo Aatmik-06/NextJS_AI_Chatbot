5249412852c7d732f2cc868e5f56bebf4540e774
Aatmik Kushwaha <adi120kush@gmail.com>
2025-09-08 21:02:02 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -54,13 +54,7 @@ export default {
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)',
-  			'tremor-full': '9999px'
-  		},
-  		fontSize: {
-  			'tremor-label': [
-  				'0.75rem'
-  			]
+  			sm: 'calc(var(--radius) - 4px)'
   		}
   	}
   },
