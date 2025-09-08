baf7d1c034e61efdbd23ce609a8c860dcebb5e89
Aatmik Kushwaha <adi120kush@gmail.com>
2025-09-08 21:11:54 +0530

    Updateapi

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
       setAnswer("Thinking....");
       try {
         const response = await axios({
-          url: "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyAmI8sN1T5SzP3AlM-5CPxFRlxFUyxJvd0", 
+          url: "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyDEY5jVFlcl08a5Zo0E5ORR-tudwVaY72E", 
           method: "post",
           data: {
             contents: [{ parts: [{ text: question }] }],
